23e064965ca9393a143e1cfb3c72e2322e658f57
ChristiansonCR2089 <ChristiansonCR2089@aguafria.org>
2018-08-28 09:10:19 -0700

    All but Fib

--- my programs/Exercise2/Chap2Exercise.java
+++ my programs/Exercise2/Chap2Exercise.java
@@ -6,6 +6,11 @@ public static void main(String[] args)
     Exercise2();
     Exercise3();
     Exercise4();
+    Exercise5();
+    Exercise6();
+    Exercise7();
+    Exercise8();
+    Exercise9();
 }
 
 public static void Exercise1()
@@ -57,9 +62,123 @@ public static void Exercise4()
 public static void Exercise5()
 {
     System.out.println("Exercise5");
-    for(int i = 1; i <= 4; i++)
+    for(int i = 1; i <= 1; i++)
     {
         System.out.println("*");
+        for(int j = 1; j <= 1; j++)
+    {
+        System.out.println("**");
+        for(int l = 1; l <= 1; l++)
+    {
+        System.out.println("***");
+        for(int t = 1; t <= 1; t++)
+    {
+        System.out.println("****");
+    }
+    }
+    }
+    }
+}
+
+    public static void Exercise6()
+{
+    System.out.println("Exercise6");
+    for(int i = 1; i <= 1; i++)
+    {
+        System.out.println("1");
+        for(int j = 1; j <= 1; j++)
+    {
+        System.out.println("22");
+        for(int l = 1; l <= 1; l++)
+    {
+        System.out.println("333");
+        for(int t = 1; t <= 1; t++)
+    {
+        System.out.println("4444");
+    {
+        System.out.println("55555"); 
+    {
+        System.out.println("666666");
+    {
+        System.out.println("7777777"); 
+    } 
+    }
+    } 
+    }
+    }
+    }
+    }
+}
+
+public static void Exercise7()
+{
+    System.out.println("Exercise7");
+    for(int i = 1; i <= 1; i++)
+    {
+        System.out.println("    1");
+        for(int j = 1; j <= 1; j++)
+    {
+        System.out.println("   2 ");
+        for(int l = 1; l <= 1; l++)
+    {
+        System.out.println("  3");
+        for(int t = 1; t <= 1; t++)
+    {
+        System.out.println(" 4");
+    {
+        System.out.println("5"); 
+    } 
+    }
+    }
+    }
+    }
+}
+
+public static void Exercise8()
+{
+    System.out.println("Exercise8");
+    for(int i = 1; i <= 1; i++)
+    {
+        System.out.println("    1");
+        for(int j = 1; j <= 1; j++)
+    {
+        System.out.println("   22");
+        for(int l = 1; l <= 1; l++)
+    {
+        System.out.println("  333");
+        for(int t = 1; t <= 1; t++)
+    {
+        System.out.println(" 4444");
+    {
+        System.out.println("55555"); 
+    } 
+    }
+    }
+    }
+    }
+}
+
+public static void Exercise9()
+{
+    System.out.println("Exercise9");
+    for(int i = 1; i <= 1; i++)
+    {
+        System.out.println("----------------------------------------");
+        for(int j = 1; j <= 1; j++)
+    {
+        System.out.println("_-^-_-^-_-^-_-^-_-^-_-^-_-^-_-^-_-^-_-^-");
+        for(int l = 1; l <= 1; l++)
+    {
+        System.out.println("1122334455667788990011223344556677889900");
+        for(int t = 1; t <= 1; t++)
+    {
+            System.out.println("--------------------------------------");
+        
+    {
+    }
+    }
+    }
+    }
     }
 }
 }
