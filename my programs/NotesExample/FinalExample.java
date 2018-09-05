8db47da6d13a5565e2da3bee517b1fc85730ac55
ChristiansonCR2089 <ChristiansonCR2089@aguafria.org>
2018-09-05 10:02:07 -0700

    final example

---
+++ my programs/NotesExample/FinalExample.java
@@ -0,0 +1,13 @@
+public class FinalExample{
+    public static final int NUM_PLAYERS=2;
+
+    public static void startgame()
+    {
+        int numofturns = NUM_PLAYERS;
+        for (;numofturns <= 100;numofturns++)
+        {
+                                                
+        }
+    }
+
+}
